d40ec58fc800ab806d6b8b38113573337ef0e4e2
강진우 <wlsdn1252@naver.com>
2023-01-14 22:23:09 +0900

    기본액션바에서 커스텀 액션바로 설정

--- app/src/main/java/com/example/drawablexmlpractice/MainActivity.kt
+++ app/src/main/java/com/example/drawablexmlpractice/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.drawablexmlpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1.기본액션바 불러내기
+        val defaultActionBar = supportActionBar!!
+
+        // 액션바 모드를 커스텀 지원으로 설정
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+        // 어떤 커스텀뷰를 설정할건지 설정
+        defaultActionBar.setCustomView(R.layout.my_custom_actionbar)
     }
 }
