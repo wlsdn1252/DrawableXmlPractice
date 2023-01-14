106a40aff0824ba4cf1cf24ed45bde1b3a51208a
강진우 <wlsdn1252@naver.com>
2023-01-14 22:26:00 +0900

    액션바 양 옆 여백 공간 제거

--- app/src/main/java/com/example/drawablexmlpractice/MainActivity.kt
+++ app/src/main/java/com/example/drawablexmlpractice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.drawablexmlpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +18,16 @@ class MainActivity : AppCompatActivity() {
 
         // 어떤 커스텀뷰를 설정할건지 설정
         defaultActionBar.setCustomView(R.layout.my_custom_actionbar)
+
+        // 커스텀뷰를 들고있는 툴바 저장
+        val toolbar = defaultActionBar.customView.parent as Toolbar
+
+        // 액션바의 여백 설정 여부
+        toolbar.setContentInsetsAbsolute(0,0)
+
+
+
+
+
     }
 }
